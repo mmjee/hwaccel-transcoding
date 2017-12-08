50fa0a7db9b2de15e1cec9189b09fa8cd69fa1be
Maharshi Mukherjee <rizumukherjee337@gmail.com>
2017-12-09 03:50:11 +0530

    Changes in argument order

--- h2642h264.sh
+++ h2642h264.sh
@@ -2,4 +2,4 @@
 
 # XXX: DELIBERATELY STOLEN FROM FFMPEG DOCS
 
-ffmpeg -hwaccel cuvid -c:v h264_cuvid -i "$1" -c:v h264_nvenc -preset slow "$2"
+ffmpeg -hwaccel cuvid -c:v h264_cuvid -i "$1" -q:a 0 -q:v 0 -c:v h264_nvenc -preset slow "$2"
